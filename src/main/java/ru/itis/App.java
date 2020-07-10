d1fbbc8bb7d09ad3247f5294f1ab6eae7c31f589
Hyperb0t <vitalij-groshev@mail.ru>
2020-07-10 17:57:54 +0300

    fixed bug2

--- src/main/java/ru/itis/App.java
+++ src/main/java/ru/itis/App.java
@@ -7,7 +7,9 @@ package ru.itis;
 public class App 
 {
     public static void main( String[] args )
+
     {
+        System.out.println("fixed bug2");
         System.out.println( "Hello World!" );
     }
 }
